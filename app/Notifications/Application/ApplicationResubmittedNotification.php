ba58dd21781b2127ec17aaecca3a0cdd6943338e
John Lei Ulit <john.ulit@oqulo.com>
2026-05-07 15:39:33 +0800

    add Queueable

--- app/Notifications/Application/ApplicationResubmittedNotification.php
+++ app/Notifications/Application/ApplicationResubmittedNotification.php
@@ -3,11 +3,12 @@
 namespace App\Notifications\Application;
 
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 use App\Models\Application\Application;
 
-class ApplicationResubmittedNotification extends Notification
+class ApplicationResubmittedNotification extends Notification implements ShouldQueue
 {
     use Queueable;
     
